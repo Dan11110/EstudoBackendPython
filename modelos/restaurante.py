a96311653ed53cbaa3ea5a341f6352b213067ece
Daniel Vieira Medeiros <119529129+Dan11110@users.noreply.github.com>
2024-09-17 12:00:35 -0300

    Update restaurante.py

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,21 +1,24 @@
 class Restaurante:
     restaurantes = []
     def __init__(self, nome, categoria, cidade, bairro, rua):
-        self.nome = nome
-        self.categoria = categoria
-        self.cidade = cidade
-        self.bairro = bairro
-        self.rua = rua
-        self.ativo = False
+        self._nome = nome.title()
+        self._categoria = categoria
+        self._cidade = cidade
+        self._bairro = bairro
+        self._rua = rua
+        self._ativo = False
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return f"\nNome: {self.nome}\nCategoria: {self.categoria}\nCidade: {self.cidade}\nBairro: {self.bairro}\nRua: {self.rua}\n"
+        return f"\nNome: {self._nome}\nCategoria: {self._categoria}\nCidade: {self._cidade}\nBairro: {self._bairro}\nRua: {self._rua}\nStatus: {self.ativo}\n"
     
     def listar_restaurantes():
             for restaurante in Restaurante.restaurantes:
                 print(restaurante)
 
+    @property
+    def ativo(self):
+         return "✅" if self._ativo else "❎"
+
 restaurante_novo = Restaurante("Teste", "Pizzaria", "Rio de janeiro", "Cachambi", "Chaves Pinheiro")
 restaurante_novo = Restaurante("Teste2", "Pizzaria Nova", "Rio de janeiro", "Cachambi", "Chaves Pinheiro")
-
