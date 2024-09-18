f42a528aca59c47c74847fa8e51bb387845f9f3e
Daniel Vieira Medeiros <119529129+Dan11110@users.noreply.github.com>
2024-09-18 15:48:59 -0300

    Update restaurante.py

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,3 +1,4 @@
+from avaliacao import Avaliacao
 class Restaurante:
     restaurantes = []
     def __init__(self, nome, categoria, cidade, bairro, rua):
@@ -6,19 +7,46 @@ class Restaurante:
         self._cidade = cidade
         self._bairro = bairro
         self._rua = rua
+        self._avaliacoes = []
         self._ativo = False
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return f"\nNome: {self._nome}\nCategoria: {self._categoria}\nCidade: {self._cidade}\nBairro: {self._bairro}\nRua: {self._rua}\nStatus: {self.ativo}\n"
+        return f"\nNome: {self._nome}\nCategoria: {self._categoria}\nCidade: {self._cidade}\nBairro: {self._bairro}\nRua: {self._rua}\nMédia: {self.media_avaliacao}\nStatus: {self.ativo}\n"
     
-    def listar_restaurantes():
-            for restaurante in Restaurante.restaurantes:
+    @classmethod
+    def listar_restaurantes(cls):
+            for restaurante in cls.restaurantes:
                 print(restaurante)
+
+    def alterar_estado_restaurante():
+        nome_restaurante = input("Digite o nome do restaurante: ")
+        restaurante_encontrado = False
+        for restaurante in Restaurante.restaurantes:
+            if nome_restaurante == restaurante._nome:
+                restaurante_encontrado = True
+                restaurante._ativo = not restaurante._ativo
+                mensagem = f"O restaurante {restaurante._nome} foi ativado com sucesso\n" if restaurante._ativo else f"O restaurante {restaurante._nome} foi desativado com sucesso\n"
+                print(mensagem)
+        if not restaurante_encontrado:
+            print("Restaurante não encontrado\n")
+
+    def receber_avaliacao(self, restaurante, cliente, nota):
+         nova_avaliacao = Avaliacao(restaurante, cliente, nota)
+         self._avaliacoes.append(nova_avaliacao)
+
+
+    @property
+    def media_avaliacao(self):
+         if not self._avaliacoes:
+            return "-" 
+         soma_avaliacoes = sum(avaliacao.nota for avaliacao in self._avaliacoes)   
+         quantidade_avaliacoes = len(self._avaliacoes)    
+         media = round(soma_avaliacoes/quantidade_avaliacoes, 1)
+         return media 
 
     @property
     def ativo(self):
          return "✅" if self._ativo else "❎"
-
-restaurante_novo = Restaurante("Teste", "Pizzaria", "Rio de janeiro", "Cachambi", "Chaves Pinheiro")
-restaurante_novo = Restaurante("Teste2", "Pizzaria Nova", "Rio de janeiro", "Cachambi", "Chaves Pinheiro")
+    
+    
